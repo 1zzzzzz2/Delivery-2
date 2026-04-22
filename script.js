77dd77540b9956b941367ea715dbb6cf2b1371e1
Gamemode <gamemode0412@qq.com>
2026-04-22 09:20:49 +0800

    add origin files

---
+++ script.js
@@ -0,0 +1,54 @@
+const form = document.getElementById("registerForm");
+
+form.addEventListener("submit", function(event) {
+
+    const name = form.elements["name"].value;
+    const email = form.elements["email"].value;
+    const phone = form.elements["phone"].value;
+    const password = form.elements["password"].value;
+    const address = form.elements["address"].value;
+    const username = form.elements["username"].value;
+
+    let isValid = true;
+
+    const namePattern = /^[A-Za-z\s]+$/;
+    if (!namePattern.test(name)) {
+        alert("Name should only contain letters and spaces");
+        isValid = false;
+    }
+
+    const emailPattern = /^[^@]+@[^@]+\.(com|cn)$/;
+    if (!emailPattern.test(email)) {
+        alert("Email must end with .com or .cn");
+        isValid = false;
+    }
+
+    const phonePattern = /^1\d{10}$/;
+    if (!phonePattern.test(phone)) {
+        alert("Phone number is not valid");
+        isValid = false;
+    }
+
+    const passPattern = /^[A-Za-z0-9]{6,}$/;
+    if (!passPattern.test(password)) {
+        alert("Password must be at least 6 letters or numbers");
+        isValid = false;
+    }
+
+    const addressPattern = /^[A-Za-z0-9\s]+$/;
+    if (!addressPattern.test(address)) {
+        alert("Address can only contain letters, numbers and spaces");
+        isValid = false;
+    }
+
+    const userPattern = /^[A-Za-z0-9]{6,}$/;
+    if (!userPattern.test(username)) {
+        alert("Username must be at least 6 letters or numbers");
+        isValid = false;
+    }
+
+        if (!isValid) {
+        event.preventDefault();
+    }
+
+});
